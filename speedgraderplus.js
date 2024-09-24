ef9d11ab75abf41c4951f5952720fe3c1938bb70
Isaac Chua <2425983+isaacchua@users.noreply.github.com>
2024-09-24 02:18:00 -0400

    Implemented hide question text and quiz comments.

--- speedgraderplus.js
+++ speedgraderplus.js
@@ -1,24 +1,26 @@
 let sgpConfig = {
-    enabled: true,
-    assignmentId: 0,
-    questionIds: [0]
+	enabled: true, // true to hide specified assignment parts, false to show everything
+	assignmentId: 0, // the assignment id
+	questionIds: [0], // the question ids to show; others will be hidden
+	hideQuestionText: true, // hides the text of the question
+	hideQuizComments: true // hides the quiz comments that follow the student's answers (not the comments panel)
 };
 globalThis.sgp = (function(config){
 	const VERSION = "1.1.0";
-    const BIND_STUDENTS_ATTEMPTS = 200;
+	const BIND_STUDENTS_ATTEMPTS = 200;
 	const BASE_CSS = "div.display_question.question { display: none; } ";
-    const STYLE_ID = "sgp_styles";
+	const STYLE_ID = "sgp_styles";
 	var find = true;
 	
 	function bindStudents(attempts = 0) {
 		let students = document.querySelectorAll("ul#students_selectmenu-menu a");
 		if (students.length === 0) { // students not yet loaded
-            if (attempts < BIND_STUDENTS_ATTEMPTS) {
-    			setTimeout(bindStudents, 100, attempts + 1);
-            }
-            else {
-                console.warn("SpeedGraderPlus: max attempts reached: unable to find student list");
-            }
+			if (attempts < BIND_STUDENTS_ATTEMPTS) {
+				setTimeout(bindStudents, 100, attempts + 1);
+			}
+			else {
+				console.warn("SpeedGraderPlus: max attempts reached: unable to find student list");
+			}
 		}
 		else {
 			for (const student of students) {
@@ -32,38 +34,38 @@ globalThis.sgp = (function(config){
 	}
 
 	function findIframe () {
-        try {
-            if (find) {
-                let iframe = document.getElementById("speedgrader_iframe");
-                if (iframe !== null) {
-                    find = false;
-                    console.log("SpeedGraderPlus: speedgrader_iframe found");
-                    if (iframe.getAttribute("src").includes("assignments/" + config.assignmentId)) {
-                        console.log("SpeedGraderPlus: assignment " + config.assignmentId + " found");
-                        iframe.contentWindow.addEventListener("load", handleIframeLoadEvent); // listen regardless, because iframe might reload
-                        if (iframe.contentDocument.readyState === "complete") { // iframe might already be loaded
-                            checkValidIframe(iframe.contentDocument);
-                        }
-                    }
-                    else {
-                        console.warn("SpeedGraderPlus: assignment " + config.assignmentId + " not found");
-                    }
-                }
-            }
-        }
-        catch (error) {
-            console.error("SpeedGraderPlus: error encountered");
-            throw error;
-        }
-        finally {
-            setTimeout(findIframe, 200); // ensure this always runs
-        }
+		try {
+			if (find) {
+				let iframe = document.getElementById("speedgrader_iframe");
+				if (iframe !== null) {
+					find = false;
+					console.log("SpeedGraderPlus: speedgrader_iframe found");
+					if (iframe.getAttribute("src").includes("assignments/" + config.assignmentId)) {
+						console.log("SpeedGraderPlus: assignment " + config.assignmentId + " found");
+						iframe.contentWindow.addEventListener("load", handleIframeLoadEvent); // listen regardless, because iframe might reload
+						if (iframe.contentDocument.readyState === "complete") { // iframe might already be loaded
+							checkValidIframe(iframe.contentDocument);
+						}
+					}
+					else {
+						console.warn("SpeedGraderPlus: assignment " + config.assignmentId + " not found");
+					}
+				}
+			}
+		}
+		catch (error) {
+			console.error("SpeedGraderPlus: error encountered");
+			throw error;
+		}
+		finally {
+			setTimeout(findIframe, 200); // ensure this always runs
+		}
 	}
 
-    function handleIframeLoadEvent (event) {
+	function handleIframeLoadEvent (event) {
 		console.log("SpeedGraderPlus: speedgrader_iframe Window.load detected");
-        checkValidIframe(event.target);
-    }
+		checkValidIframe(event.target);
+	}
 	
 	function checkValidIframe (doc) {
 		if (doc.location.href === "about:blank") {
@@ -76,51 +78,61 @@ globalThis.sgp = (function(config){
 	}
 	
 	function applyStyles (doc) {
-        var css;
+		var css;
 
-        if (config.enabled) {
-            // hide all question blocks
-            css = BASE_CSS;
+		if (config.enabled) {
+			// hide all question blocks
+			css = BASE_CSS;
 
-            // show all selected question blocks
-            for (const questionId of config.questionIds) {
-                css += "div#question_" + questionId + ", ";
-            }
-            css += ":not(*) { display: block; }";
+			// show all selected question blocks
+			for (const questionId of config.questionIds) {
+				css += "div#question_" + questionId + ", ";
+			}
+			css += ":not(*) { display: block; } ";
 
-            console.log("SpeedGraderPlus: applying styles: " + css);
-        }
-        else {
-            css = "";
-            console.log("SpeedGraderPlus: disabled, clearing styles");
-        }
+			// hide question text
+			if (config.hideQuestionText) {
+				css += "div.question_text { display: none; } ";
+			}
 
-        getStyles(doc).textContent = css;
+			// hide quiz comments
+			if (config.hideQuizComments) {
+				css += "div.quiz_comment { display: none; } ";
+			}
+
+			console.log("SpeedGraderPlus: applying styles: " + css);
+		}
+		else {
+			css = "";
+			console.log("SpeedGraderPlus: disabled, clearing styles");
+		}
+
+		getStyles(doc).textContent = css;
 
 		console.log("SpeedGraderPlus: all done");
 	}
 
-    function getStyles (doc) {
-        let style = doc.getElementById(STYLE_ID);
-        if (style === null) {
-            style = doc.createElement("style");
-            style.id = STYLE_ID;
-            doc.head.appendChild(style);
-        }
-        return style;
-    }
+	function getStyles (doc) {
+		let style = doc.getElementById(STYLE_ID);
+		if (style === null) {
+			style = doc.createElement("style");
+			style.id = STYLE_ID;
+			doc.head.appendChild(style);
+		}
+		return style;
+	}
 
 	if (document.location.href.includes("speed_grader")) {
 		console.log("SpeedGraderPlus: find iframe");
 		document.getElementById("prev-student-button").addEventListener("click", handleApplyEvent);
 		document.getElementById("next-student-button").addEventListener("click", handleApplyEvent);
 		bindStudents();
-        setTimeout(findIframe, 200);
+		setTimeout(findIframe, 200);
 	}
 
-    return {
-    	version: VERSION,
-        reapply: handleApplyEvent,
-        config: sgpConfig
-    };
+	return {
+		version: VERSION,
+		reapply: handleApplyEvent,
+		config: sgpConfig
+	};
 })(sgpConfig);
